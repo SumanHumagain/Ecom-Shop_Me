8402c4841e1f0a4ae7f13efc6cf03389713495aa
unknown <returnzerosh@gmail.com>
2024-01-20 22:53:00 +0545

    top no

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -141,7 +141,7 @@ const createProductReview = asyncHandler(async (req, res) => {
   // @route   GET /api/products/top
   // @access  Public
   const getTopProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({}).sort({ rating: -1 }).limit(10);
+    const products = await Product.find({}).sort({ rating: -1 }).limit(4);
   
     res.status(200).json(products);
   });
