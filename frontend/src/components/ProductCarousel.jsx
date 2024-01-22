72ee7608bf4f185649217362054fe9d4c581571a
unknown <returnzerosh@gmail.com>
2024-01-22 11:58:15 +0545

    design changes

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -13,7 +13,7 @@ const ProductCarousel = () => {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image style={{ height: '20vw', width:'18vw'}} src={product.image} alt={product.name} fluid />
+            <Image  style={{ height: '20vw', width:'18vw'}} src={product.image} alt={product.name} fluid />
             <Carousel.Caption className='carousel-caption'>
               <h2 className='text-white text-right'>
                 {product.name} (${product.price})
